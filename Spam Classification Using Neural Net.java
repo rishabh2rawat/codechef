f2caa4e87c652ea5709db1d4e6b7ff3a6672c433
Rishabh Rawat <rishabhricharawat@gmail.com>
2018-01-20 15:08:21 +0530

    input value

---
+++ Spam Classification Using Neural Net.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class xyz
+{
+	public static void main(String args[])
+	{
+		Scanner scan=new Scanner(System.in);
+		int t=scan.nextInt();
+		for(int i=0;i<t;i++)
+		{
+			int n=scan.nextInt();
+			int minx=scan.nextInt();
+			int maxx=scan.nextInt();
+			int w=scan.nextInt();
+			int b=scan.nextInt();
+
+			int count=0;
+			fot(int i=minx;i<=maxx;i++)
+			{
+				fot(int j=0;j< )
+			}
+		}
+	}
+}
